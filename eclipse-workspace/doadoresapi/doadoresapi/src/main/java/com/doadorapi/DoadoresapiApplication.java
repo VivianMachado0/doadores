9291dab4a9c5f5b94e54f3c2850e6cedf5090440
Vivian Machado <vivianmachadoifmg@gmail.com>
2025-05-29 14:28:21 -0300

    Configura Dockerfile e propriedades para banco Postgres

--- eclipse-workspace/doadoresapi/doadoresapi/src/main/java/com/doadorapi/DoadoresapiApplication.java
+++ eclipse-workspace/doadoresapi/doadoresapi/src/main/java/com/doadorapi/DoadoresapiApplication.java
@@ -2,8 +2,10 @@ package com.doadorapi;
 
 import java.util.Collections;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DoadoresapiApplication {
@@ -13,5 +15,12 @@ public class DoadoresapiApplication {
 	    SpringApplication app = new SpringApplication(DoadoresapiApplication.class);
 	    app.setDefaultProperties(Collections.singletonMap("server.port", port != null ? port : "8080"));
 	    app.run(args);
+	}
+
+	@Bean
+	public ApplicationRunner runner() {
+	    return args -> {
+	        System.out.println("Aplicação inicializada em " + System.currentTimeMillis());
+	    };
 	}
 }
